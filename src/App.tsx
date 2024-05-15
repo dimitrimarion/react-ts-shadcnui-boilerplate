5ae4bd72a574351afb3eccca0e084482c2069812
Dimitri Marion <dimitri.marion@gmail.com>
2024-05-15 12:44:19 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
